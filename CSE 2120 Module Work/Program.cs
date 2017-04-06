732562f2e6673265b7c203592a268e2161ebb8d6
John Berger <jwberger@shaw.ca>
2017-04-06 08:55:03 -0600

    Switched which file had what to make the names sensical.

--- CSE 2120 Module Work/Program.cs
+++ CSE 2120 Module Work/Program.cs
@@ -1,15 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CSE_2120_Module_Work
+namespace HelloWorld
 {
-    class Program
+    public class Hello
     {
-        static void Main(string[] args)
+        private static List<String> list = new List<String>();
+        private static String choice;
+        public static void Main(String[] args)
         {
+            makeChoice();
+            if (choice.Contains("yes"))
+            {
+                makeChoice();
+            }
+            if (choice.Contains("no"))
+            {
+                foreach (String i in list)
+                {
+                    foreach (String o in i.Split(' '))
+                    {
+                        Console.WriteLine(o);
+                    }
+                }
+            }
+            Console.Read();
+        }
+        public static void makeChoice()
+        {
+            Console.WriteLine("Enter a sentence:");
+            list.Add(Console.ReadLine());
+            Console.WriteLine("Do you want to enter another sentence?");
+            choice = Console.ReadLine();
+            choice = choice.ToLower();
         }
     }
 }
