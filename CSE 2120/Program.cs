c929d94f74b14ce819b2256e918dc550667f64f0
John Berger <jwberger@shaw.ca>
2017-04-25 09:41:12 -0600

    Made a project program

--- CSE 2120/Program.cs
+++ CSE 2120/Program.cs
@@ -8,8 +8,18 @@ namespace CSE_2120
 {
     class Program
     {
+        private static String input;
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter some words:");
+            input = Console.ReadLine();
+            Storage.Store(input);
+            Storage.Call(false);
+            Console.WriteLine("Enter another sentence for word use comparison:");
+            input = Console.ReadLine();
+            Storage.Store(input);
+            Storage.Call(true);
+            Console.Read();
         }
     }
 }
---
+++ CSE 2120/Storage.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSE_2120
+{
+    class Storage
+    {
+        static ArrayList al = new ArrayList();
+        static ArrayList last = new ArrayList();
+        private static int counter = 0;
+        public static void Store(String input)
+        {
+            foreach (String i in input.Split(' '))
+            {
+                al.Add(i);
+            }
+        }
+        public static void Call(bool first)
+        {
+            if (first)
+            {
+                foreach (String i in al)
+                {
+                    Console.WriteLine(i + "\t" + last[counter]);
+                    last.Add(i);
+                    counter++;
+                }
+            }
+            if (!first)
+            {
+                foreach (String i in al)
+                {
+                    Console.WriteLine(i);
+                    last.Add(i);
+                }
+                al.Clear();
+            }
+            
+        }
+    }
+}
