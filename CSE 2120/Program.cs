56242fe3f3757150a1264a209fbbc73f7cbed309
John Berger <jwberger@shaw.ca>
2017-04-25 10:13:01 -0600

    Made encryption and stuff

--- CSE 2120/Program.cs
+++ CSE 2120/Program.cs
@@ -11,14 +11,37 @@ namespace CSE_2120
         private static String input;
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter some words:");
+            Console.WriteLine("Do you want to encrypt some stuff, or compare some other stuff?");
             input = Console.ReadLine();
-            Storage.Store(input);
-            Storage.Call(false);
-            Console.WriteLine("Enter another sentence for word use comparison:");
-            input = Console.ReadLine();
-            Storage.Store(input);
-            Storage.Call(true);
+            switch(input.ToLower())
+            {
+                case "encrypt":
+                    const int keySize = 1024;
+                    string publicAndPrivateKey;
+                    string publicKey;
+
+                    Secure.GenerateKeys(keySize, out publicKey, out publicAndPrivateKey);
+
+                    string text = "Test";
+                    string encrypted = Secure.EncryptText(text, keySize, publicKey);
+                    string decrypted = Secure.DecryptText(encrypted, keySize, publicAndPrivateKey);
+                    Console.WriteLine("Encrypted: {0}", encrypted);
+                    Console.WriteLine("Decrypted: {0}", decrypted);
+                    break;
+                case "compare":
+                    Console.WriteLine("Enter some words:");
+                    input = Console.ReadLine();
+                    Storage.Store(input);
+                    Storage.Call(false);
+                    Console.WriteLine("Enter another sentence for word use comparison:");
+                    input = Console.ReadLine();
+                    Storage.Store(input);
+                    Storage.Call(true);
+                    break;
+                default:
+                    Console.WriteLine("Choose either \"encypt\" or \"compare\".");
+                    break;
+            }
             Console.Read();
         }
     }
